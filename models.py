91b2111801aab3b88a2e505fba91b202aef41b2c
EffieSanjer <FerganaZ5>
2022-08-13 16:40:05 +0800

    test

--- models.py
+++ models.py
@@ -29,7 +29,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Ivan Vasilevich!" + str(datetime.now())
+    return "Hello, Ivan Vasilevich!"
 
 Base = declarative_base()
 
